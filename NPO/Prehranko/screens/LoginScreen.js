73ebec725d21db8c600aa0c742da011b300b6281
MartinFerk <martinferk434@gmail.com>
2025-05-20 14:50:59 +0200

    Koncan railway

--- NPO/Prehranko/screens/LoginScreen.js
+++ NPO/Prehranko/screens/LoginScreen.js
@@ -8,7 +8,7 @@ export default function LoginScreen({ navigation }) {
   const handleLogin = async () => {
     console.log("➡️ Poskušam se prijaviti z:", email, password);
     try {
-      const res = await fetch('http://192.168.1.158:5000/api/auth/login', {
+      const res = await fetch('https://prehranko-production.up.railway.app/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
