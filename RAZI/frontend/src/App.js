508de657a078cdee0f47dba23dfb2a4c966500d9
MartinFerk <martinferk434@gmail.com>
2025-05-26 23:16:07 +0200

    Novi zacetek spletnega vmensika

--- RAZI/frontend/src/App.js
+++ RAZI/frontend/src/App.js
@@ -1,28 +1,23 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [name, setName] = useState('');
-  const [response, setResponse] = useState('');
-
-  const sendData = async () => {
-    const res = await fetch('https://prehranko-production.up.railway.app/api', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name }),
-    });
-
-    const data = await res.json();
-    setResponse(data.message);
-
-    
-  };
-
   return (
-    <div>
-      <h1>Frontend → Backend</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={sendData}>Pošlji</button>
-      <p>Odgovor: {response}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
