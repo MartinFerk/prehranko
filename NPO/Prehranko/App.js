6d17961f956d6c452fc9bb31f4a9cf7d5a2ad6ac
Jan Kupčič <kupcic.jan@gmail.com>
2025-05-28 11:58:08 +0200

    MQTT Development

--- NPO/Prehranko/App.js
+++ NPO/Prehranko/App.js
@@ -1,4 +1,11 @@
 // App.js
+import { Buffer } from 'buffer';
+import process from 'process';
+import 'react-native-get-random-values';
+
+global.Buffer = Buffer;
+global.process = process;
+
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 
