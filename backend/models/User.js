cbe577dffb4e56ed1676689c251f0129546809c4
MartinFerk <martinferk434@gmail.com>
2025-05-29 20:31:55 +0200

    Shranjevanje znacilk

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  // Shrani seznam značilk (vektorjev)
-  features: {
-    type: [[Number]], // array of arrays of floats
-    required: false    // optional, če še ni 2FA aktivirano
-  },
-  pending2FA: { type: Boolean, default: false }
+  faceImages: { type: [String], default: [] },
+  faceEmbeddings: {
+  type: [[Number]], // 2D array, npr. [[0.1, 0.2, ...], [0.3, 0.5, ...]]
+  default: [],
+},
 });
 
 module.exports = mongoose.model('User', userSchema);
