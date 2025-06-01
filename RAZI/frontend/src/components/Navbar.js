3b9ff4c90bbf8e4bfa2a75d6242577a0c644e6ab
Paskal Trstenjak <paskal.trstenjak@hotmail.com>
2025-06-02 00:31:20 +0200

    Updated Navbar with correct links

--- RAZI/frontend/src/components/Navbar.js
+++ RAZI/frontend/src/components/Navbar.js
@@ -10,10 +10,13 @@ const Navbar = () => {
           <Link to="/" className="nav-link">Statistika</Link>
         </li>
         <li>
-          <Link to="/login" className="nav-link">Prijava</Link>
+          <Link to="/profile" className="nav-link">Profil</Link>
         </li>
         <li>
-          <Link to="/profile" className="nav-link">Profil</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+        </li>
+         <li>
+          <Link to="/register" className="nav-link">Register</Link>
         </li>
       </ul>
     </nav>
