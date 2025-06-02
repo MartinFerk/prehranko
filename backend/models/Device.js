635f22c3636a7d4d0f864ca29751d7f590614642
Paskal Trstenjak <paskal.trstenjak@hotmail.com>
2025-06-03 01:05:08 +0200

    Updated auth.js with mqtt functionalities

--- backend/models/Device.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const deviceSchema = new mongoose.Schema({
-  clientId: { type: String, required: true, unique: true },
-  status: { type: String, enum: ['connected', 'disconnected'], default: 'disconnected' },
-  lastUpdated: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Device', deviceSchema);
