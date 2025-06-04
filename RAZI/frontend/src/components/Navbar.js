2d0c9d070164ed34935e65cb41fde6d34629b16e
Jan Kupčič <kupcic.jan@gmail.com>
2025-06-04 11:33:21 +0200

    Greyed out Profile if user not logged in

--- RAZI/frontend/src/components/Navbar.js
+++ RAZI/frontend/src/components/Navbar.js
@@ -3,23 +3,30 @@ import { Link } from 'react-router-dom';
 import '../styles.css';
 
 const Navbar = () => {
+  const isLoggedIn = localStorage.getItem('loggedIn') === 'true';
+
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/" className="navbar-link">Domov</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/profile" className="navbar-link">Profil</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/login" className="navbar-link">Prijava</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/register" className="navbar-link">Registracija</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-link">Domov</Link>
+          </li>
+          <li className="navbar-item">
+            {isLoggedIn ? (
+                <Link to="/profile" className="navbar-link">Profil</Link>
+            ) : (
+                <span className="navbar-link disabled-link">Profil</span>
+            )}
+          </li>
+          <li className="navbar-item">
+            <Link to="/login" className="navbar-link">Prijava</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/register" className="navbar-link">Registracija</Link>
+          </li>
+        </ul>
+      </nav>
   );
 };
+
 export default Navbar;
